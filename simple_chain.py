5b69f7f75d3fefef2ee2fdb67f43a339789b580b
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 18:31:46 +0530

    Simple Chain code Added

---
+++ simple_chain.py
@@ -0,0 +1,64 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+
+load_dotenv()
+
+hf_token = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+
+llm = HuggingFaceEndpoint(
+    
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation",
+    huggingfacehub_api_token=hf_token,
+    
+)
+
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+chain.get_graph().print_ascii()
+
+#      +-------------+       
+#      | PromptInput |
+#      +-------------+
+#             *
+#             *
+#             *
+#     +----------------+
+#     | PromptTemplate |
+#     +----------------+
+#             *
+#             *
+#             *
+#    +-----------------+
+#    | ChatHuggingFace |
+#    +-----------------+
+#             *
+#             *
+#             *
+#    +-----------------+
+#    | StrOutputParser |
+#    +-----------------+
+#             *
+#             *
+#             *
+# +-----------------------+
+# | StrOutputParserOutput |
+# +-----------------------+
